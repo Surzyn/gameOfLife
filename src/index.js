005dd576cbc962a9da3714594c0560a099a79bec
Bartłomiej Surzyński <bsurzynski@gmail.com>
2018-06-03 18:08:56 +0200

    change styles, add performance

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import style from "./main.css";
 import renderer from './gui';
 import { conwayRule } from './rule';
 
-var arr = generate(100);
+var arr = generate(200);
 
 renderer(arr);
 
 setInterval(() => {
+    var t0 = performance.now();
     arr = conwayRule(arr);
+    var t1 = performance.now();
     renderer(arr);
-}, 100)
+    var t2 = performance.now();
+    console.log(`calculate:  ${t1 - t0}, render  ${t2 - t1}`);
+}, 200)
 
 function generate(size) {
     var arr = [];
