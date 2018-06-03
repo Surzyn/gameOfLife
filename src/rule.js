dd6ae95919e15cacbcb8d23c10896b03a70ef0e8
Bartłomiej Surzyński <bsurzynski@gmail.com>
2018-06-03 18:10:39 +0200

    change typo

--- src/rule.js
+++ src/rule.js
@@ -3,15 +3,15 @@ export function conwayRule(arr) {
     for (let i = 0; i < arr.length; i++) {
         nextGeneration[i] = [];
         for (let j = 0; j < arr[i].length; j++) {
-            var liveNeightbours = countLiveNeightbours(arr, i, j);
+            var lifeNeightbours = countLifeNeightbours(arr, i, j);
             nextGeneration[i][j] = false;
             if (!arr[i][j]) {
-                if (liveNeightbours == 3) {
+                if (lifeNeightbours == 3) {
                     nextGeneration[i][j] = true;
                 }
             }
             else if (arr[i][j]) {
-                if (liveNeightbours == 2 || liveNeightbours == 3) {
+                if (lifeNeightbours == 2 || lifeNeightbours == 3) {
                     nextGeneration[i][j] = true;
                 }
             }
@@ -22,15 +22,15 @@ export function conwayRule(arr) {
     return nextGeneration;
 }
 
-function countLiveNeightbours(arr, i, j) {
-    var liveNeightbours = 0;
-    if (i > 0 && arr[i - 1][j]) { liveNeightbours += 1; }
-    if (i < arr.length - 1 && arr[i + 1][j]) { liveNeightbours += 1; }
-    if (arr[i][j - 1]) { liveNeightbours += 1; }
-    if (arr[i][j + 1]) { liveNeightbours += 1; }
-    if (i > 0 && arr[i - 1][j - 1]) { liveNeightbours += 1; }
-    if (i > 0 && arr[i - 1][j + 1]) { liveNeightbours += 1; }
-    if (i < arr.length - 1 && arr[i + 1][j - 1]) { liveNeightbours += 1; }
-    if (i < arr.length - 1 && arr[i + 1][j + 1]) { liveNeightbours += 1; }
-    return liveNeightbours;
+function countLifeNeightbours(arr, i, j) {
+    var lifeNeightbours = 0;
+    if (i > 0 && arr[i - 1][j]) { lifeNeightbours += 1; }
+    if (i < arr.length - 1 && arr[i + 1][j]) { lifeNeightbours += 1; }
+    if (arr[i][j - 1]) { lifeNeightbours += 1; }
+    if (arr[i][j + 1]) { lifeNeightbours += 1; }
+    if (i > 0 && arr[i - 1][j - 1]) { lifeNeightbours += 1; }
+    if (i > 0 && arr[i - 1][j + 1]) { lifeNeightbours += 1; }
+    if (i < arr.length - 1 && arr[i + 1][j - 1]) { lifeNeightbours += 1; }
+    if (i < arr.length - 1 && arr[i + 1][j + 1]) { lifeNeightbours += 1; }
+    return lifeNeightbours;
 }
